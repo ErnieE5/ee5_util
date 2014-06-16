4bedbfdff89b06b85ab122e06a59bd1b142315ea
Ernie Ewert <ernie@ee5.net>
2014-06-15 23:16:03 -0700

    checkpoint

--- tests/main_old.cpp
+++ tests/main_old.cpp
@@ -595,92 +595,6 @@ struct ThreadpoolTest
 };
 
 
-#include <numeric>
-
-template<typename L,size_t iterations = 800000>
-void run_lock_test(TP& p)
-{
-    printf("rlt %-25.25s ",typeid(L).name()); fflush(stdout);
-
-    m_stopwatch_f       sw;
-    L                   lock;
-    std::vector<size_t> times;
-    std::atomic_size_t  a;
-    std::atomic_size_t  c;
-    long double         d = 0;
-    size_t              f[100] = { };
-
-    times.reserve(iterations);
-
-    for(size_t x = 0;x < iterations;++x)
-    {
-        RC dd = s_ok();
-        do
-        {
-            dd = p.Async( [&]()
-            {
-                long double ttf  = 0;
-
-                for(size_t q = 0;q < 100;q++)
-                {
-                    ttf += ThreadpoolTest::Factorial(q*2);
-                }
-
-                us_stopwatch_s swt;
-                lock.lock();
-                times.push_back(swt.delta());
-
-                static thread_local size_t tid = a++;
-
-                size_t iter = c++;
-                f[tid]++;
-                d += ttf;
-
-                lock.unlock();
-
-                // for(size_t j = 0;j < 1;j++)
-                // {
-                //     RC qq = s_ok();
-
-                //      do
-                //      {
-                //         qq = p.Async( [&]()
-                //         {
-                //             long double ttg = ThreadpoolTest::Factorial(25);
-                //             lock.lock();
-                //             d += ttg;
-                //             lock.unlock();
-                //         });
-                //      }
-                //      while( qq != s_ok() );
-                // }
-            });
-        }
-        while( dd != s_ok() );
-    }
-
-    size_t xx;
-    do
-    {
-        xx = p.Pending();
-//        printf("%8lu %8lu\n",xx,c.load());
-    }
-    while(xx>0);
-
-    size_t sum = std::accumulate(times.begin(),times.end(),0);
-    auto mm = std::minmax_element(times.begin(),times.end());
-    printf("-%10lu-",sum);
-
-    printf("%12.8f m %9lu / ", sw.delta(), c.load());
-    //printf("%20lu us %9lu / ", sw.delta(), c.load());
-    for(size_t b = 0;b < p.Count() ;b++)
-    {
-        printf("%2lu:%9lu ",b,f[b]);
-    }
-    printf("%lu us (%lu,%lu)\n",sum/iterations,*mm.first,*mm.second); fflush(stdout);
-}
-
-
 
 
 static constexpr size_t threads = 8;
@@ -708,17 +622,6 @@ RC FunctionTests()
     // grow_test();
 
     // return s_ok();
-
-    static constexpr size_t iterations = threads * 100000000;
-
-    run_lock_test<std::mutex,iterations>           (p);
-    run_lock_test<spin_posix,iterations>           (p);
-    run_lock_test<spin_mutex,iterations>           (p);
-    run_lock_test<spin_barrier,iterations>         (p);
-    run_lock_test<spin_shared_mutex_t,iterations>  (p);
-
-    return s_ok();
-
 
     // Scalar Types
     //
@@ -960,7 +863,7 @@ void App()
 
     p.Start();
 
-    FunctionTests();
+//    FunctionTests();
 
     p.Shutdown();
 
